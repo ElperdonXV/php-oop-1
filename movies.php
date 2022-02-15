b5d91822eba3cf3332e055ce6b5bfc50b20c8d46
ElperdonXV <davidemartinografico@gmail.com>
2022-02-15 15:41:59 +0100

    ok

--- movies.php
+++ movies.php
@@ -5,6 +5,41 @@ class movie{
     public $date;
     public $author;
     public $duration;
+    public function __construct($title, $date, $author){
+        $this->title = $title;
+        $this->date = $date;
+        $this->author = $author;
+    }
+    public function setTitle($title){
+        $this->title = $title;
+    }
+    public function getTitle($title){
+        return $this->title;
+    }
+    public function setGenre($genre){
+        $this->genre = $genre;
+    }
+    public function getGenre($genre){
+        return $this->genre;
+    }
+    public function setDate($date){
+        $this->date = $date;
+    }
+    public function getDate($date){
+        return $this->date;
+    }
+    public function setAuthor($author){
+        $this->author = $author;
+    }
+    public function getAuthor($author){
+        return $this->author;
+    }
+    public function setDuration($duration){
+        $this->duration = $duration;
+    }
+    public function getDuration($duration){
+        return $this->duration;
+    }
 }
 
 ?>
